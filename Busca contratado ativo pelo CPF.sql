6885a526162327f90ac8c27d344495ea4a0675c3
Bruno Mendes <102331277+BrunoSMendes89@users.noreply.github.com>
2022-03-25 12:08:37 -0300

    Update Busca contratado ativo pelo CPF.sql

--- Busca contratado ativo pelo CPF.sql
+++ Busca contratado ativo pelo CPF.sql
@@ -1,5 +1,5 @@
 SELECT CON_CdiContratado, CON_DssNome, CON_NusCICNumero, CON_DtdAdmissao, CON_CdiVinculo, VIN_D1sVinculo
 FROM Contratados
 INNER JOIN Vinculos ON (CON_CdiVinculo = VIN_CdiVinculo)
-WHERE CON_NusCICNumero = '019.775.690-56'
+WHERE CON_NusCICNumero = 'XXX.XXX.XXX-XX'
 AND CON_CdiSituacao = 1
